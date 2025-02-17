3ce83e116b0a057e8218bd421810250702790d98
Fortune Ogunsusi <99345157+ddboy19912@users.noreply.github.com>
2025-02-17 02:59:48 +0100

    feat: installed and setup shadcdn components

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  css: {
+    devSourcemap: true,
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+    preprocessorOptions: {
+      scss: {
+        // additionalData: `@import "./src/styles/index.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
