095d06e449ec9ba7a9a068eaa27b7a62baa8ee6e
Fortune Ogunsusi <99345157+ddboy19912@users.noreply.github.com>
2025-02-22 04:10:29 +0100

    chore: uploaded required assets and icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,21 @@
+import AvatarPlaceholder from "./AvatarPlaceholder.png";
+import LoanIllustration from "./LoanIllustration.png";
 import LogoIllustration from "./LoginIllustration.svg";
 import Logo from "./Logo.svg";
+import PeopleIllustration from "./PeopleIllustration.png";
+import SavingsIllustration from "./SavingsIllustration.png";
+import UsersIllustration from "./UsersIllustration.png";
+import DeleteUserIcon from "./delete.png";
+import UserIcon from "./user.png";
 
-export { Logo, LogoIllustration };
+export {
+  AvatarPlaceholder,
+  DeleteUserIcon,
+  LoanIllustration,
+  Logo,
+  LogoIllustration,
+  PeopleIllustration,
+  SavingsIllustration,
+  UserIcon,
+  UsersIllustration,
+};
